35829d319c9f8c17f5b6e3c013b688a7ee105c7e
citadelrugby-personal <citadelrugby@gmail.com>
2026-05-04 14:51:12 -0400

    index.css addition

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles/global.css'
+import App from './App.jsx'
+import './index.css'   // optional — remove if you don’t have this file
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
